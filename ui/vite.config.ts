ff0ba088cdfbf3ce4ec65e0e09bc977131122c46
0x5341 <107183580+0x5341@users.noreply.github.com>
2026-02-28 19:16:45 +0900

    add tailwindcss

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,6 +2,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { playwright } from "@vitest/browser-playwright";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +13,13 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     browser: {
       provider: playwright(),
